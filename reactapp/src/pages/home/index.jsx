444465ec259ed371691662d05f4a77a5439b0aee
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-01-02 21:54:10 -0300

    criei um outro estado que vai armazenar os estudantes dentro de um array, em seguida, criei um objeto que irá adicionar os estudantes, pegando o nome do setStudentname e o tempo de um método nativo que pega datas

--- reactapp/src/pages/home/index.jsx
+++ reactapp/src/pages/home/index.jsx
@@ -6,6 +6,22 @@ import { useState } from 'react';
 function home() {
 
   const [studentName, setStudentName] = useState()  //ao usar o state passamos a variável e a função que atualzia o estado
+  const [students, setStudents] = useState([]);
+
+  function AddStudent() {
+    const newStudent ={
+      name: setStudentName,
+      time: new Date().toLocaleDateString("pt-br",
+       {
+        hour: '2-digit',
+        minute: '2-digit',
+        second:'2-digit'
+      })
+    };
+
+    setStudents([newStudent])
+  }
+
   return (
     <div className="container">
      <h1>Nome: {studentName}</h1>
@@ -16,9 +32,11 @@ function home() {
 
      
      <button>Teste</button>
-     <Card name="Reinaldo" time="17:35"/>
-     <Card name="Dario" time="17:36"/>
-     <Card name="Lucas" time="17:39" />
+     {
+      students.map(student => <Card name={student.name} time={student.time}/>)
+      
+      }
+     
     </div>
    
   )
